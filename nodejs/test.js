dc828485dafbc6c2b477a3bb5bac098c07d7e583
puritys <puritys@case.pt>
2013-09-07 02:27:25 +0800

    Move test file directory

--- nodejs/test.js
+++
@@ -1,5 +0,0 @@
-var b = require("./phplike");
-
-var res = b.exec("php /home/puritys/test.php");
-
-console.log(res);
---
+++ tests/exec.js
@@ -0,0 +1,19 @@
+var phplike = require ('./../nodejs/phplike');
+
+var str = "";
+for (var i =0; i < 20; i++) {
+    str += "1";
+    if (i%1000 == 0) {
+        str += "\n";
+    }
+}
+console.log("start to run exec");
+
+var res = phplike.exec("echo '" + str + "'");
+
+console.log(res);
+
+res = phplike.exec("ls / | wc -l ");
+
+console.log("Counting dir = " + res);
+

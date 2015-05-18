1b457078e7f182048a0c5001cbcd02a92fcfdff9
puritys <pipi00000@gmail.com>
2015-05-19 01:02:26 +0800

    fix for mac compile

--- system/socket/src/socket.h
+++ system/socket/src/socket.h
@@ -5,6 +5,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#ifndef MSG_CONFIRM
+#define MSG_CONFIRM 0x800
+#endif
+
 int phplikeSocketConnect(char *hostname, int port);
 void phplikeSocketSend(int sockfd, char *msg, unsigned int len);
 char* phplikeSocketReceive(int sockfd, unsigned int length, unsigned int *resLength);

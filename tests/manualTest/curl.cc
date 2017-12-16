ecc5cce6db5973552098d00e9d91d7eccbee4fa3
puritys <puritys.chen@gmail.com>
2017-12-16 07:11:49 +0000

    Add curl test script

--- tests/manualTest/curl.cc
+++ tests/manualTest/curl.cc
@@ -1,6 +1,7 @@
 /*
  * g++ curl.cc -lcurl -o curl.out
  */
+
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
@@ -46,48 +47,33 @@ size_t recvResponse(void *ptr, size_t size, size_t nmemb, struct string2 *s )  {
 
 }
 
-
-int main(int argc, const char *argv[])
-{
-    if (curl_global_init(CURL_GLOBAL_ALL) != CURLE_OK) {
-       fprintf(stderr, "curl_global_init() failed\n");
-       return 0;
-    }
-    char httpVersion[5];
-    string resHeader = "";
-    string resContent = "";
-    size_t contentLength;
-    char *resContentPointer;
-
+CURLcode setBasicCurl(CURL *curl, string method, string2 *resH, string2 *resC) {/*{{{*/
     CURLcode res;
-    CURL *curl;
-    contentLength  = 0;
-    string method = "GET";
     std::transform(method.begin(), method.end(), method.begin(), ::toupper);
-
-    string url = "https://www.google.com.tw/";
-    curl = curl_easy_init();
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
     curl_easy_setopt(curl, CURLOPT_HEADER, 1L);
     curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
-    struct string2 resH;
-    init_string(&resH);
+
+    init_string(resH);
     res = curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, recvResponse);
-    curl_easy_setopt(curl, CURLOPT_HEADERDATA, &resH);
+    curl_easy_setopt(curl, CURLOPT_HEADERDATA, resH);
 
-    struct string2 resC;
-    init_string(&resC);
+    init_string(resC);
     res = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, recvResponse);
-    res = curl_easy_setopt(curl, CURLOPT_WRITEDATA, &resC);
+    res = curl_easy_setopt(curl, CURLOPT_WRITEDATA, resC);
 
-    res = curl_easy_perform(curl);
-    long http_code = 0;
-    curl_easy_getinfo (curl, CURLINFO_RESPONSE_CODE, &http_code);
+}/*}}}*/
 
-    string headerTmp;
+string handleResponse(CURL *curl, CURLcode res, string2 resH, string2 resC) {/*{{{*/
+    size_t contentLength  = 0;
     size_t contentStartPosition = 0;
-
+    char *resContentPointer;
+    string resHeader = "";
+    string resContent = "";
+    string headerTmp = "";
     if (!res) {
+        long http_code = 0;
+        curl_easy_getinfo (curl, CURLINFO_RESPONSE_CODE, &http_code);
+
         stringstream ss;
         ss << http_code;
         resHeader = ss.str() + "\r\n";
@@ -115,11 +101,36 @@ int main(int argc, const char *argv[])
         contentLength = resC.len - contentStartPosition;
         resContentPointer = resC.ptr + contentStartPosition;
     }
+    return resContent;
 
+}/*}}}*/
+
+void req1() {
+    CURL *curl;
+    curl = curl_easy_init();
+    struct string2 resH;
+    struct string2 resC;
+    string method = "GET";
+    CURLcode res = setBasicCurl(curl, method, &resH, &resC);
+    string url = "https://www.google.com.tw/";
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+    res = curl_easy_perform(curl);
+    string resp = handleResponse(curl, res, resH, resC);
+    //cout << "response = " << resp;
     curl_easy_cleanup(curl);
+
+}
+
+
+int main(int argc, const char *argv[])
+{
+    if (curl_global_init(CURL_GLOBAL_ALL) != CURLE_OK) {
+       fprintf(stderr, "curl_global_init() failed\n");
+       return 0;
+    }
+    req1();
     curl_global_cleanup();
-
-
     return 0;
 }
 
